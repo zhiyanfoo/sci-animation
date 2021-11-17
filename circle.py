eedd0f6f39cf9be0abc96c41dcb7f1ca3d0e080e
Zhiyan Foo <zhiyanfoo@gmail.com>
2021-11-18 05:55:46 +0800

    a

--- circle.py
+++ circle.py
@@ -6,19 +6,56 @@ import random
 def quadratic_interpolation(a):
     return (-(a - 1.5) ** 2 + 3)/ 6
 
+def linear_interpolation_diff(start_iteration, end_iteration, start_position, end_position):
+    iter_diff = 1/(end_iteration - start_iteration)
+    m = (end_position - start_position) * iter_diff
+    def li(current_iteration):
+        if current_iteration < start_iteration:
+            return 0
+
+        if current_iteration > end_iteration:
+            return 0
+
+        return m
+
+    return li
+
+def linear_interpolation(start_iteration, end_iteration, start_position, end_position):
+    iter_diff = 1/(end_iteration - start_iteration)
+    m = (end_position - start_position) * iter_diff
+    def li(current_iteration):
+        if current_iteration < start_iteration:
+            return start_position
+
+        if current_iteration > end_iteration:
+            return end_position
+
+        return current_iteration * m + start_position
+
+    return li
+
+
 
 class Circle:
-    def __init__(self, origin, radius, inner_shell_radius):
-        self.origin = np.array(origin, dtype=float)
-        self.radius = radius
+    def __init__(self, start_position, end_position, inner_shell_radius, outer_shell_radius,
+            start_volatility, end_volatility, grow_frame_start, grow_frame_end):
+        self.origin = np.array(start_position, dtype=float)
+        self.start_position = np.array(start_position, dtype=float)
+        self.end_position = np.array(end_position, dtype=float)
         self.inner_shell_radius = inner_shell_radius
-        self.choose_new_axis()
+        self.outer_shell_radius = outer_shell_radius
         self.matplotlib_circle_outer =  plt.Circle(self.origin, self.inner_shell_radius,
                 color="green", zorder=10)
-        self.matplotlib_circle_inner =  plt.Circle(self.origin, self.radius, color="aqua",
+        self.matplotlib_circle_inner =  plt.Circle(self.origin, self.outer_shell_radius, color="aqua",
                 zorder=1)
-        for i in range(2):
-            self.vibrate(True, i)
+        self.grow_frame_start = grow_frame_start
+        self.grow_frame_end = grow_frame_end
+        self.li_vibration_radius = linear_interpolation(
+                grow_frame_start + 50, grow_frame_end + 50, start_volatility, end_volatility)
+        self.li_d = linear_interpolation_diff(grow_frame_start + 50 + 50, grow_frame_end + 50 + 50, self.start_position, self.end_position)
+        self.li_height_gain = linear_interpolation_diff(grow_frame_start + 150, grow_frame_end + 150, 0, 20)
+        self.choose_new_axis(0)
+
 
     def get_displacment_vector(self):
         return self.displacement_radius * np.array([cos(self.displacement_angle), sin(self.displacement_angle)])
@@ -35,11 +72,13 @@ class Circle:
         else:
             self.origin += v
 
-        self.origin += np.array([0.0, 0.1])
+        self.origin += self.li_d(i)
+        self.origin += np.array([0, self.li_height_gain(i)])
 
         # print(self.matplotlib_circle.circle)
 
-    def choose_new_axis(self):
-        self.displacement_radius = random.uniform(0.6, 0.9)
+    def choose_new_axis(self, i):
+        vibration_radius = self.li_vibration_radius(i)
+        self.displacement_radius = random.uniform(vibration_radius/2, vibration_radius)
         self.displacement_angle = random.uniform(0, 1) * 2 * pi
         self.displacment_vector = self.get_displacment_vector()
--- hot_air_ballon_main.py
+++ hot_air_ballon_main.py
@@ -10,11 +10,14 @@ from circle import Circle
 
 def setup(ax):
     inner_circles = []
-    for i in range(-30, 31, 5):
-        for j in range(-30, 31, 5):
+    for i in range(-30, 30, 5):
+        for j in range(-30, 30, 5):
             if i ** 2 + j ** 2 > 30**2:
                 continue
-            circle = Circle((50 + i, 50 + j), 3, 0.6)
+            circle = Circle(
+                    (50 + i, 50 + j),
+                    (50 + 1.3 *i, 50 + 1.3 * j),
+                     0.6, 3, 0.5, 1.3, 0, 200)
             inner_circles.append(circle)
 
     return inner_circles
@@ -26,13 +29,10 @@ def setup_single(ax):
 
     return inner_circles
 
-
 def main():
     fig = plt.figure(dpi=150)
     ax = plt.axes(xlim=(-10, 110), ylim=(-10, 110))
     plt.gca().set_aspect('equal', adjustable='box')
-    img = mpimg.imread("assets/balloon.png")
-    imgplot = plt.imshow(img)
     # plt.show()
     # plt.axis('equal')
     # plt.axis('square')
@@ -40,35 +40,38 @@ def main():
     # circles = setup_single()
     # circles = [plt.Circle((0, 0), 2)]
     patches = ([circle.matplotlib_circle_inner for circle in circles]) + ([circle.matplotlib_circle_outer for circle in circles])
+
+    def next_frame_gen(circle):
+        frame_index = 0
+        vibration_length = 4
+        start = random.randint(0, vibration_length)
+        while True:
+            for i in range(start, vibration_length):
+                circle.vibrate(True, frame_index)
+                i = yield
+
+            for i in range(vibration_length):
+                circle.vibrate(False, frame_index)
+                frame_index = yield
+
+            for i in range(start):
+                circle.vibrate(True, frame_index)
+                frame_index = yield
+
+            circle.choose_new_axis(frame_index)
+
+    circle_generators = list(map(next_frame_gen, circles))
+    for cg in circle_generators:
+        next(cg)
+
     def init_func():
         for circle in circles:
             circle.plot_2d(ax)
-
         return patches
-
-    def next_frame_gen(circle):
-        vibration_length = 4
-        while True:
-            for i in range(2, 4):
-                circle.vibrate(True, i)
-                yield
-
-            for i in range(vibration_length):
-                circle.vibrate(False, i)
-                yield
-
-            for i in range(vibration_length - 2):
-                circle.vibrate(True, i)
-                yield
-            yield
-
-            circle.choose_new_axis()
-
-    circle_generators = list(map(next_frame_gen, circles))
 
     def next_frame(i):
         for circle in circle_generators:
-            next(circle)
+            circle.send(i)
         return patches
 
     animate(init_func, next_frame, fig)
